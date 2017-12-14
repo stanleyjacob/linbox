db658b391618d47e27db1c6022e2f26ddf287d53
Vincent Neiger <vincent.neiger@unilim.fr>
2017-12-15 00:10:09 +0100

    some updates

--- examples/approx-basis.C
+++ examples/approx-basis.C
@@ -22,9 +22,40 @@ void bench_approximant_basis(
 		size_t n,
 		size_t d,
 		size_t f,
-		size_t t ) {
+		size_t t,
+		bool resUpdate ) {
+
 	typedef PolynomialMatrix<PMType::polfirst,PMStorage::plain,Field> MatrixP;
 	typedef PolynomialMatrix<PMType::matfirst,PMStorage::plain,Field> PMatrix;
+
+#ifdef VERBOSE_ON
+	switch (f) {
+		case 0:
+			cout << "~~~ Benchmarking algos MBASIS, PMBASIS, new MBASIS, new PMBASIS ~~~" << endl;
+			break;
+		case 1:
+			cout << "~~~ Benchmarking algo MBASIS ~~~" << endl;
+			break;
+		case 2:
+			cout << "~~~ Benchmarking algo PMBASIS ~~~" << endl;
+			break;
+		case 3:
+			cout << "~~~ Benchmarking NEW algo MBASIS ~~~" << endl;
+			break;
+		case 4:
+			cout << "~~~ Benchmarking NEW algo PMBASIS ~~~" << endl;
+			break;
+		case 5:
+			cout << "~~~ Benchmarking algo POPOV_PMBASIS ~~~" << endl;
+			break;
+		default:
+			cout << "!!! Invalid function provided through argument '-f' !!!" << endl;
+			cout << "(should be among: 0:mbasis; 3:newmbasis; 2:pmbasis; 3:newpmbasis.)" << endl;
+			cout << "Exiting..." << endl;
+			return;
+	}
+	cout<<"--> input polynomial matrix: dimensions " << m << " x " << n << " and degree " << d << endl;
+#endif
 
 #ifdef EXTRA_VERBOSE_ON
 	cout << sys << endl;
@@ -99,7 +130,7 @@ void bench_approximant_basis(
 		cout << "TIME (mbasis), creating basis: " << chrono.usertime() << " s" << endl;
 #endif // EXTRA_TIMINGS_ON
 		chrono.clear(); chrono.start(); // time the approximant basis computation
-		AppBas.mbasis(appbas, *sys, d, shift);
+		AppBas.mbasis(appbas, *sys, d, shift, resUpdate);
 		chrono.stop();
 #ifdef TIMINGS_ON
 		cout << "TIME (mbasis), computing basis: " << chrono.usertime() << " s" << endl;
@@ -129,6 +160,29 @@ void bench_approximant_basis(
 #endif
 		delete sys;
 	}
+
+	if (f==0 || f==5){ // Popov PM-Basis
+		PMatrix *sys = new PMatrix(GF, m, n, d);	
+		// set the Serie at random
+		for (size_t k=0;k<d;++k)
+			for (size_t i=0;i<m;++i)
+				for (size_t j=0;j<n;++j)
+					rand.random(sys->ref(i,j,k));
+		vector<int> shift(m,0);
+		chrono.clear(); chrono.start(); // time the matrix creation
+		PMatrix appbas(GF, m, m, d+1);
+		chrono.stop();
+#ifdef EXTRA_TIMINGS_ON
+		cout << "TIME (popov_pmbasis), creating basis: " << chrono.usertime() << " s" << endl;
+#endif // EXTRA_TIMINGS_ON
+		chrono.clear(); chrono.start(); // time the approximant basis computation
+		AppBas.popov_pmbasis(appbas, *sys, d, shift);
+		chrono.stop();
+#ifdef TIMINGS_ON
+		cout << "TIME (popov_pmbasis), computing basis: " << chrono.usertime() << " s" << endl;
+#endif
+		delete sys;
+	}
 }
 
 int main(int argc, char** argv){
@@ -142,7 +196,8 @@ int main(int argc, char** argv){
 	size_t p=0; // size of the base field (default: random)
 	size_t b=20; // entries bitsize
 	size_t t=0; // threshold for pm-basis
-	size_t f=0; // which function to benchmark: 0:mbasis; 3:newmbasis; 2:pmbasis; 3:newpmbasis.
+	size_t f=0; // which function to benchmark: 0:all; 1:mbasis; 2:pmbasis; 3:newmbasis; 4:newpmbasis; 5:popov_pmbasis
+	bool r=true; // whether to continuously update the residual, or recompute it
 	long seed = time(NULL);
 
 	static Argument args[] = {
@@ -151,40 +206,15 @@ int main(int argc, char** argv){
 		{ 'd', "-d d", "Set degree of  matrix series to d.", TYPE_INT, &d },
 		{ 'p', "-p p", "Set cardinality of prime field (if 0 --> will be random)", TYPE_INT, &p },
 		{ 'b', "-b b", "Set bitsize of the matrix entries (if random prime)", TYPE_INT, &b },
-		{ 'f', "-f f", "Set the targeted function to benchmark (0:mbasis; 3:newmbasis; 2:pmbasis; 3:newpmbasis.", TYPE_INT, &f },
+		{ 'f', "-f f", "Set the targeted function to benchmark (0:mbasis; 1:mbasis; 2:pmbasis; 3:newmbasis; 4:newpmbasis; 5:popov_pmbasis.", TYPE_INT, &f },
 		{ 't', "-t t", "Set degree threshold for pm-basis", TYPE_INT, &t },
+		{ 'r', "-r r", "Set resUpdate for mbasis", TYPE_BOOL, &r },
 		{ 's', "-s s", "Set the random seed to a specific value", TYPE_INT, &seed},
 		END_OF_ARGUMENTS
 	};
 
 	parseArguments (argc, argv, args);
 	size_t logd=integer((uint64_t)d).bitsize();
-
-#ifdef VERBOSE_ON
-	switch (f) {
-		case 0:
-			cout << "~~~ Benchmarking algos MBASIS, PMBASIS, new MBASIS, new PMBASIS ~~~" << endl;
-			break;
-		case 1:
-			cout << "~~~ Benchmarking algo MBASIS ~~~" << endl;
-			break;
-		case 2:
-			cout << "~~~ Benchmarking algo PMBASIS ~~~" << endl;
-			break;
-		case 3:
-			cout << "~~~ Benchmarking NEW algo MBASIS ~~~" << endl;
-			break;
-		case 4:
-			cout << "~~~ Benchmarking NEW algo PMBASIS ~~~" << endl;
-			break;
-		default:
-			cout << "!!! Invalid function provided through argument '-f' !!!" << endl;
-			cout << "(should be among: 0:mbasis; 3:newmbasis; 2:pmbasis; 3:newpmbasis.)" << endl;
-			cout << "Exiting..." << endl;
-			return 0;
-	}
-	cout<<"--> input polynomial matrix: dimensions " << m << " x " << n << " and degree " << d << endl;
-#endif
 
 	if (p>0 or (b<26 && logd>b-4)) {
 		if (p>0) {
@@ -202,7 +232,7 @@ int main(int argc, char** argv){
 		}
 		SmallField GF(p);
 		typename SmallField::RandIter rand(GF,0,seed);
-		bench_approximant_basis(GF,rand,m,n,d,f,t);
+		bench_approximant_basis(GF,rand,m,n,d,f,t,r);
 	}
 	else if (b < 26){ // here, logd<=b-4
 		RandomFFTPrime Rd(1<<b,seed);
@@ -212,7 +242,7 @@ int main(int argc, char** argv){
 #endif
 		SmallField GF(p);
 		typename SmallField::RandIter rand(GF,0,seed);
-		bench_approximant_basis(GF,rand,m,n,d,f,t);
+		bench_approximant_basis(GF,rand,m,n,d,f,t,r);
 	}
 	else {
 		RandomPrimeIterator Rd(b,seed);	
@@ -223,7 +253,7 @@ int main(int argc, char** argv){
 #endif
 		LargeField GF(pp);
 		typename LargeField::RandIter G(GF,b,seed);
-		bench_approximant_basis(GF,G,m,n,d,f,t);
+		bench_approximant_basis(GF,G,m,n,d,f,t,r);
 	}
 	
 	return 0;
